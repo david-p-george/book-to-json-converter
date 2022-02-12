5733c0075132b088a123af270e6b36b1064d34bb
David George <off.davidgeorge@gmail.com>
2022-02-12 18:10:14 +0530

    almost all done

--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -12,7 +12,6 @@ const InputSection = () => {
 	}
 
 	const handleRemove = (index) => {
-		// chapters.splice(index, 1);
 		let currentChapters = [...chapters];
 		currentChapters.splice(index, 1);
 		setChapters(currentChapters);
@@ -21,20 +20,73 @@ const InputSection = () => {
 	const handleRemoveSection = (i, sectionNo) => {
 		let currentChapters = [...chapters];
 		let indexChapter = currentChapters[i];
-		currentChapters[i] = indexChapter.sections.filter(obj => obj !== sectionNo);
+		let updatedSections = indexChapter.sections?.filter(obj => obj.no !== sectionNo);
+		currentChapters[i].sections = updatedSections;
 		setChapters(currentChapters);
 	}
 
+	const handleAddSection = (i) => {
+		let currentChapters = [...chapters];
+		let indexChapter = currentChapters[i];
+		indexChapter.sections?.push({ no: currentChapters[i].sections?.length + 1 });
+		currentChapters[i] = indexChapter;
+		setChapters(currentChapters);
+	}
+
+	const lowLevelConvert = () => {
+    const bookName = document.getElementById('bookName').value;
+    const responseObj = { type: 'book', name: bookName, data: [] };
+
+    Array.from(document.querySelectorAll('#chapter')).map((chapter) => {
+      let chapterText = chapter.querySelector('#chapterText').value;
+      let chapterNo = chapter.querySelector('#chapterNo').value;
+
+      let chapterObj = { 
+				type: 'chapter', 
+				chapterNo: chapterNo, 
+				chapterText: chapterText, noOfSections: 0, sections: [] 
+			};
+
+      Array.from(chapter.querySelectorAll('#section')).map((section) => {
+        let sectionText = section.querySelector('#sectionText').value;
+				let sectionObj = { type: 'section', text: sectionText };
+          
+        chapterObj.noOfSections += 1;
+        chapterObj.sections.push(sectionObj);
+      })
+
+      responseObj.data.push(chapterObj);
+  })
+
+  return JSON.stringify(responseObj, null, 2);
+	}
+
+	const handleConvert = () => {
+		document.getElementById('outputPRE').innerHTML = lowLevelConvert();
+	}
+	
+	const handleReset = () => {
+		setChapters([{ chapterNo: 1, sections: [{ no: 1 }] }]);
+		document.getElementById('bookName').value = '';
+		document.getElementById('sectionText').value = '';
+		document.getElementById('chapterText').value = '';
+	}
+
+
   return (
 		<div className='w-[500px] h-[700px] bg-white overflow-y-auto overflow-x-auto'>
-			<div className='flex justify-between w-full'>
+			<div className='flex justify-around w-full'>
 				<FormControl className='ml-2 mt-2'>
 					<FormLabel htmlFor='bookName'><span className='font-semibold text-lg'>Book Name</span></FormLabel>
 					<Input id='bookName' name='bookName' htmlSize={8} width='auto' />
 				</FormControl>
 
-				<Button className='mr-2 mt-2' leftIcon={<RepeatIcon />}>
-					Convert
+				<Button className='mr-2 mt-2' onClick={handleReset}>
+					Reset
+				</Button>
+
+				<Button className='mr-2 mt-2' leftIcon={<RepeatIcon w={7} h={7} paddingLeft='2' />} onClick={handleConvert}>
+					<span className='pr-2'>Convert</span>
 				</Button>
 			</div>
 
@@ -52,14 +104,14 @@ const InputSection = () => {
 			</div>
 
 			<div className='w-full flex flex-col justify-center items-center'>
-				{chapters.map((chapter, i) => (
+				{chapters?.map((chapter, i) => (
 					<div className='flex flex-col border-2 mt-3 mb-3 w-[450px]' id='chapter'>
 						<div className='flex justify-end mr-2'>
 							<Button 
 								colorScheme='gray' 
 								leftIcon={<AddIcon />} 
 								variant='solid' 
-								onClick={() => chapter.sections.push({ no: chapter.sections.length + 1 })}
+								onClick={() => handleAddSection(i)}
 								className='mt-2'>
 									Section
 							</Button>
@@ -77,15 +129,15 @@ const InputSection = () => {
 
 							<FormControl>
 								<FormLabel htmlFor='chapterNo'><span className='font-semibold text-lg'>No</span></FormLabel>
-								<Input id='chapterNo' name='chapterNo' isReadOnly={true} value={chapter.chapterNo} htmlSize={1} width='auto' />
+								<Input id='chapterNo' name='chapterNo' isReadOnly={true} value={chapter?.chapterNo} htmlSize={1} width='auto' />
 							</FormControl>
 						</div>
 
 						<div>
-							<Heading size='lg' className='ml-2 mt-2'>Sections</Heading>
+							<Heading size='lg' className='ml-2 mt-2 mb-2'>Sections</Heading>
 
-							{chapter.sections.map((section) => (
-								<div className='flex flex-row w-[400px] justify-center items-center mb-4 mt-4 ml-5 rounded-lg bg-gray-100'>
+							{chapter?.sections?.map((section) => (
+								<div className='flex flex-row w-[400px] justify-center items-center mb-4 mt-4 ml-5 rounded-lg bg-gray-100' id='section'>
 									<FormControl className='ml-2'>
 										<FormLabel htmlFor='sectionText'><span className='font-semibold text-lg'>Text</span></FormLabel>
 										<Input id='sectionText' name='sectionText' htmlSize={30} width='auto' className='mb-3' borderColor='blue.200' />
@@ -94,7 +146,7 @@ const InputSection = () => {
 									<Button className='mr-2 mt-6 ml-3' bgColor='blue.100'>
 										<DeleteIcon 
 											color='red.500' 
-											onClick={() => chapter.sections = chapter.sections.filter(obj => obj !== section.no )} 
+											onClick={() => handleRemoveSection(i, section?.no)} 
 										/>
 									</Button>
 								</div>
